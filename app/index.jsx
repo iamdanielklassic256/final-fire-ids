0f4fcba5030b216a76d101beb7a8c2a046978a30
Okumu Daniel Comboni <okumucomboni@gmail.com>
2025-04-14 18:24:40 +0300

    Welcome screen updated

--- app/index.jsx
+++ app/index.jsx
@@ -11,25 +11,25 @@ const WelcomeScreen = () => {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView className="flex-1 bg-gray-900">
+      <SafeAreaView className="flex-1 bg-white">
         <ScrollView contentContainerStyle={{ flexGrow: 1 }} className="px-6">
           <View className="flex-1 justify-between py-10">
             {/* Logo */}
-            <View className="items-center mb-10">
+            <View className="items-center mb-1">
               <Image
                 source={logo}
-                className="bg-white w-40 h-40 rounded-full"
+                className="w-[400px] h-[400px] rounded-full"
                 resizeMode="contain"
               />
              
             </View>
 
             {/* Hero Text */}
-            <View className="mb-12">
-              <Text className="text-white text-center text-2xl font-extrabold leading-tight mb-4">
-                Welcome to {"\n"}Bible Church Connect.
+            <View className="mb-2">
+              <Text className="text-gray-900 text-center text-2xl font-extrabold leading-tight mb-4">
+                You're welcome to {"\n"}Bible Church Connect.
               </Text>
-              <Text className="text-gray-400 text-center text-base leading-6">
+              <Text className="text-gray-900 text-center text-base leading-6">
                 Explore the Bible, reflect daily, listen to the Word, and grow with a global faith community — all in one place.
               </Text>
             </View>
@@ -38,7 +38,7 @@ const WelcomeScreen = () => {
             <View className="space-y-4">
               <TouchableOpacity
                 onPress={handleRoute}
-                className="bg-[#f27c22] py-4 rounded-xl shadow-xl"
+                className="bg-[#f26b22] py-4 rounded-xl shadow-xl"
               >
                 <Text className="text-white text-center text-lg font-bold">Get Started</Text>
               </TouchableOpacity>
